328a0431e56f371596ad8bcc21e30cf95b219e16
fedepaloz <palozzifederico@gmail.com>
2022-07-10 19:33:31 +0200

    esercizio finito

--- script.js
+++ script.js
@@ -11,18 +11,81 @@ const container = document.getElementById("container")
 
 //faccio 100 celle con un ciclo for
 
-button.addEventListener('click', cellGen)
-    
 
-function cellGen () {for (let i = 1; i <= 100; i++) {
-    const cell = document.createElement('div')
-    cell.className = "cell"
-    cellContainer.appendChild(cell)
-    cell.innerText = i
-    cell.addEventListener('click' , function ()
-    {cell.classList.add("clicked")})
-    
-}
-    
+let score = 0
+
+
+function cellGen() {
+    for (let i = 1; i <= 100; i++) {
+        const cell = document.createElement('div')
+        cell.className = "cell"
+        cellContainer.appendChild(cell)
+        cell.innerText = i
+        cell.addEventListener('click', function () {
+            if (this.classList.contains('clicked')) return
+            cell.classList.add("clicked")
+            const isGameOver = checkGameOver(cell, bombs);
+            if (!isGameOver) score++;
+            console.log(score)
+        })
+    }
+
 }
 
+
+
+//funzione gameover
+
+function checkGameOver(cell, bombs) {
+    const cellNumber = parseInt(cell.innerText);
+    if (bombs.includes(cellNumber)) {
+        cell.classList.add("bomb");
+        gameOver(score, false)
+        return true
+    } else {
+        cell.classList.add('safe');
+        if (score + 1 === winningPoints) {
+            gameOver(winningPoints, true)
+            return true
+        }
+        return false;
+    }
+}
+
+function gameOver(score, hasWon) {
+    let message = ''
+    if(hasWon){
+        message = 'hai vinto, punteggio è ' + score;
+    }
+    else {message = 'hai perso, punti totalizzati ' + score}
+
+    alert (message)
+}
+
+
+    button.addEventListener('click', cellGen)
+
+const totalCells = 100
+const totalBombs = 16
+
+//punteggio massimo 
+const winningPoints = totalCells - totalBombs
+
+//funzione per generare i numeri delle bombe
+
+const bombs = generateBombs(totalBombs, totalCells)
+
+function generateBombs(totalBombs, totalCells) {
+    const bombs = [];
+
+    while (bombs.length < totalBombs) {
+        let randomNumber
+        do {
+            randomNumber = Math.floor(Math.random() * totalCells) + 1
+        } while (bombs.includes(randomNumber));
+        bombs.push(randomNumber)
+    }
+    return bombs
+}
+
+console.log(bombs)
